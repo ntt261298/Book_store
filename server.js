b35f166edfbfc485369c3741ea774c6ab5d367de
ntt261298 <ntt261298@gmail.com>
2019-01-20 00:43:09 +0700

    Version 1.0.0

--- server.js
+++ server.js
@@ -94,14 +94,14 @@ app.use('/admin/product', book);
 app.use('/admin/cart', cart);
 app.use('/admin/manager', manager);
 
-// app.use(express.static(path.join(__dirname, 'public')));
 // Serve static assets if in production
-// if(process.env.NODE_ENV === 'production') {
-//   app.use(express.static('client/build'));
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   })
-// }
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 const port = process.env.PORT || 5000;
 
